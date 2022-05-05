ebcb9c85f1bac1ec2b1c58370e9f2696f2d28b0d
Linux User <aaron@fmad.io>
2022-05-05 21:02:24 +0900

    set ES cluster to have 0 replicates on mapping

--- pcap2json/es_mapping.sh
+++ pcap2json/es_mapping.sh
@@ -1,2 +1,6 @@
 #!/usr/local/bin/bash
 /usr/local/bin/curl -H "Content-Type: application/json"  -XPUT "192.168.255.110:9200/pcap2json_index?pretty" --data-binary "@mapping.json" | jq 
+
+# disable replicas
+/usr/local/bin/curl -H 'Content-Type: application/json' -X PUT http://192.168.255.110:9200/_template/default -d '{"index_patterns": ["*"],"order": -1,"settings": {"number_of_shards": "1","number_of_replicas": "0"}}' | jq
+
