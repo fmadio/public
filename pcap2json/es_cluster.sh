fbdf26c1948a6d9e373880ab2efebb06c1c41e6b
Linux User <aaron@fmad.io>
2022-05-07 17:04:35 +0900

    disable es cluster disks thresholds

--- pcap2json/es_cluster.sh
+++ pcap2json/es_cluster.sh
@@ -3,5 +3,9 @@
 # various default settings for the cluster
 
 # disable replicas
-/usr/local/bin/curl -H 'Content-Type: application/json' -X PUT http://192.168.255.110:9200/_template/default -d '{"index_patterns": ["*"],"order": -1,"settings": {"number_of_shards": "1","number_of_replicas": "0"}}'
+/usr/local/bin/curl -H 'Content-Type: application/json' -X PUT http://192.168.255.110:9200/_template/default -d '{"index_patterns": ["*"],"order": -1,"settings": {"number_of_shards": "1","number_of_replicas": "0"}}' | jq
+
+
+# disable disk treshold 
+/usr/local/bin/curl -H 'Content-Type: application/json' -X PUT http://192.168.255.110:9200/_cluster/settings  -d '{"transient": {"cluster.routing.allocation.disk.threshold_enabled": false}}' | jq
 
